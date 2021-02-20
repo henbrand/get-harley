1f8e24b30b4ed5b37b6602e13713fb3b23f872fe
Hen <henrietteb@theodo.co.uk>
2021-02-20 13:45:21 +0000

    (api) add cors middleware

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const practitioners = require("./data/practitioners.json");
 const specialities = require("./data/specialities.json");
-
+const cors = require("cors");
 const express = require("express");
+
 const app = express();
 const port = 3030;
+app.use(cors());
 
 app.get("/practioners", (req, res) => {
   res.json(practitioners);
