5d1d1139f002d6cbc548a63ee4dfc5aa59ed459d
Hen <henrietteb@theodo.co.uk>
2021-02-20 21:58:00 +0000

    (styling) add primary theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import LuxonUtils from "@date-io/luxon";
+import {
+  createMuiTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@material-ui/core/styles";
 
 import { Home } from "./pages/Home";
+import { Colors } from "./styles/colors";
 
+const theme = responsiveFontSizes(
+  createMuiTheme({
+    palette: {
+      primary: {
+        main: Colors.primary,
+      },
+    },
+  })
+);
 function App() {
   return (
-    <MuiPickersUtilsProvider utils={LuxonUtils}>
-      <Home />
-    </MuiPickersUtilsProvider>
+    <ThemeProvider theme={theme}>
+      <MuiPickersUtilsProvider utils={LuxonUtils}>
+        <Home />
+      </MuiPickersUtilsProvider>
+    </ThemeProvider>
   );
 }
 
