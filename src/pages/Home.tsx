3d6d4bec3c3551d5ad7da05df63a1f3c49c2a87d
Hen <henrietteb@theodo.co.uk>
2021-02-21 21:30:35 +0000

    (clean) add form to home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,66 +1,19 @@
-import { FunctionComponent, useCallback, useState } from "react";
-import { DateTime } from "luxon";
+import { FunctionComponent } from "react";
 import styled from "styled-components";
 
 import logo from "../logo.png";
-import { Button } from "../components/atoms/Button";
-import { Speciality } from "../apiClient/types";
-import { DateTimeSlotPicker } from "../components/DateTimeSlotPicker";
-import { useSpecialities } from "../hooks/useSpecialities";
-import { useAvailableTimeslot } from "../hooks/useAvailableTimeslot";
+import { FormTabs } from "../components/FormTabs";
 
 const Logo = styled.img`
-  height: 100%;
-  width: 50%;
+  margin: 10px;
+  width: 35%;
 `;
 
 export const Home: FunctionComponent = () => {
-  const [chosenSpeciality, setChosenSpeciality] = useState<Speciality>();
-  const [selectedDateTime, setSelectedDateTime] = useState<DateTime>();
-  const { specialities, error } = useSpecialities();
-  const { sendSelectedTimeslot } = useAvailableTimeslot();
-  const [practitioners, setPractitioners] = useState<Practitioner[]>();
-
-  const handleConfirm = useCallback(async () => {
-    const practitionerAvailabilty = await sendSelectedTimeslot({
-      specialityId: chosenSpeciality?.specialityId,
-      selectedDate: {
-        date: selectedDateTime?.startOf("day"),
-        dateTime: selectedDateTime,
-        dateString: selectedDateTime?.toISODate(),
-      },
-    });
-
-    setPractitioners(practitionerAvailabilty);
-  }, [chosenSpeciality?.specialityId, sendSelectedTimeslot, selectedDateTime]);
-
   return (
     <div>
       <Logo src={logo} className="get-harley-logo" alt="logo" />
-      <h1>Select a reason for booking:</h1>
-      {specialities?.map((speciality) => {
-        return (
-          <Button
-            key={speciality.specialityId}
-            selected={
-              chosenSpeciality?.specialityId === speciality.specialityId
-            }
-            buttonText={speciality.name}
-            onClick={() => setChosenSpeciality(speciality)}
-            square
-          />
-        );
-      })}
-      {error && <p>{error}</p>}
-      {chosenSpeciality && (
-        <DateTimeSlotPicker
-          selectedDateTime={selectedDateTime}
-          setSelectedDateTime={setSelectedDateTime}
-        />
-      )}
-      {chosenSpeciality && selectedDateTime && (
-        <Button buttonText="Confirm" onClick={handleConfirm} />
-      )}
+      <FormTabs />
     </div>
   );
 };
